f7915507555f78527ccb525a32554cb08596c26e
Pepe <phferreira479@gmail.com>
2023-09-25 13:11:10 +0200

    update

--- temp.py
+++ temp.py
@@ -1,11 +1,23 @@
-from sympy import N
+import numpy as np
+import matplotlib.pyplot as plt
 
-p = N(1/3, 16)
-print(p)
-p = N(1/3, 4)
-print(p)
-a = N(0.1, 30)
-print(a)
-b = N(0.1, 5)
-print(b)
-print(a-b)
+t_sim, c_sim = np.loadtxt("./data/sim_data.txt").T
+t_exp, c_exp = np.loadtxt("./data/exp_data.txt").T
+
+# Linear interpolation
+c_sim_new = np.interp(t_exp, t_sim, c_sim)
+diff = np.abs(c_exp - c_sim_new)
+
+# Plot the solution
+plt.figure(figsize=(4.5,4.5))
+plt.subplot(2, 1, 1)
+plt.plot(t_exp, c_exp, 'b-x', label='c_exp')
+plt.plot(t_exp, c_sim_new, 'r-o', label='c_sim_new')
+plt.legend()
+
+plt.subplot(2, 1, 2)
+plt.plot(t_exp, diff); plt.show()
+
+# Compute the L2-norm
+norm = np.linalg.norm(diff)
+print(norm)
