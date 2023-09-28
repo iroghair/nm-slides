4b53dc259af400af3c673168c4163d6f1f12ae0d
Pepe <phferreira479@gmail.com>
2023-09-28 10:19:16 +0200

    Used these files

--- temp.py
+++ temp.py
@@ -1,37 +1,13 @@
-import numpy as np
-from numpy.linalg import norm
+from scipy.optimize import linprog
 
-# Define the function
-def solve_jacobi(A, b, tol=1e-2):
-    
-    # Set initial guess
-    x = b + 1e-16
-        
-    # Initialize variables
-    x_diff = 1
-    N = len(A)
-    it_jac = 1
-    
-    while ( x_diff > tol and it_jac < 1000 ):
-        x_old = np.copy(x)
-        for i in range(N):
-            # Sum off-diagonal*x_old
-            offDiagonalIndex = np.concatenate((np.arange(0, i), np.arange(i+1, N)))
-            Aij_Xj = np.dot(A[i, offDiagonalIndex], x_old[offDiagonalIndex])
+c = [-5, -4, -6]
+A = [[1, -1, 1], [3, 2, 4], [3, 2, 0]]
+b = [20, 42, 30]
+bounds = [(0, None), (0, None), (0, None)]
 
-            # Compute new x value
-            x[i] = (b[i] - Aij_Xj) / A[i, i]
-        it_jac += 1
-        x_diff = np.linalg.norm((x - x_old), ord=2)
-        
-    # Print number of iterations
-    print(it_jac)
-    
-    return x, it_jac
-
-
-A = np.array([[4, -1, 0, 0], [-1, 4, -1, 0], [0, -1, 4, -1], [0, 0, -1, 3]])
-b = np.array([15, 10, 10, 10])
-x = np.linalg.solve(A,b)
-x_ = solve_jacobi(A,b, 1e-6)[0]
-print(x, x_)
+res = linprog(c, A_ub=A, b_ub=b, bounds=bounds)
+x = res.x
+fun = res.fun
+slack = res.slack
+success = res.success
+print(x, fun, slack, success)
