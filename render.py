7aa44569662cdb92134c5c375b4d125929e139e1
Ivo Roghair <i.roghair@tue.nl>
2023-10-05 09:53:52 +0200

    Add separate view and clean options

--- render.py
+++ render.py
@@ -6,14 +6,13 @@ import re
 # AUX endings
 aux = (".toc", ".snm", ".out", ".nav", ".aux", ".log", ".vrb", ".dvi", ".fls", ".gz")
 outfile = Path("./Slides.pdf")
+latex_exec = "pdflatex"
 
-
-def main(argv):
+def render(argv):
     # Collecting the filenames 
     filenames = [Path(f).stem for f in argv]
     f_sep_commas = ",".join([f for f in filenames])
     f_with_path = ",".join(["sections/%s"%f for f in filenames])
-
 
     # Writing includes to include.tex
     if filenames:
@@ -29,18 +28,22 @@ def main(argv):
     # Check if the logfile exists
     logfile = ''
     # Run latex compiler until output does not change
-    while not bool(re.search(fr"File `{outfile.stem}.out' has not changed","|".join(logfile))):
-        sp.run(["pdflatex", "-jobname=%s"%outfile.stem,"main.tex"]) 
+    while not re.search(fr"File `{outfile.stem}.out' has not changed","|".join(logfile)):
+        sp.run([latex_exec, "-jobname=%s"%outfile.stem,"main.tex"]) 
         with open(f"{outfile.stem}.log","r") as f:
             logfile = f.readlines()
 
-    # Deleting the aux files 
-    in_dir =tuple(map(Path,os.listdir()))
+    sp.run([latex_exec, "-jobname=%s"%outfile.stem,"main.tex"])
+    
+def clean(argv):
+    # Delete the auxiliary files 
+    in_dir = tuple(map(Path,os.listdir()))
     in_sections = tuple(f for f in Path("./sections").iterdir() if f.is_file())
     for f in in_dir+in_sections:
         if f.suffix in aux:
             f.unlink()
-    
+
+def view(argv):
     # Determine the operating system (Windows or Linux) and opening the resulting file
     if os.name == "posix":
         # Linux or macOS
@@ -48,10 +51,15 @@ def main(argv):
     else:
         # Windows
         command = ["start", outfile.name]
-    
-    #sp.run(command, shell=True)
 
+    sp.run(command)
+    
 
 if __name__ == "__main__":
     argv = [a for a in sys.argv if a!="python" and a[0]!="-"]
-    main(argv[1:])
+    if 'clean' in argv:
+        clean(argv[1:])
+    elif 'view' in argv:
+        view(argv[1])
+    else:
+        render(argv[1:])
